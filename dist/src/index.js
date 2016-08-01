c922019bdb69e7016efad41240d73bd0e68c41e0
Pipe <mynameispipe@gmail.com>
2016-08-01 20:54:09 +0700

    compile source

--- dist/src/index.js
+++ dist/src/index.js
@@ -1,3 +1,14 @@
-console.log('run');
+"use strict";
+var TestClass = (function () {
+    function TestClass() {
+        console.log("create test class");
+    }
+    TestClass.prototype.testGet = function () {
+        console.log("test get");
+    };
+    return TestClass;
+}());
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.default = TestClass;
 
 //# sourceMappingURL=index.js.map
